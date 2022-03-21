f43be75dfb1dc2be6097c5cfd2cdb600709da851
Nadinefiona <nadinefiona9@gmail.com>
2022-03-21 23:40:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector('.burger');
+let ulLink = document.querySelector('ul');
+console.log(ulLink);
+
+burger.addEventListener("click" ,[] =>{
+    ulLink.classList.toggle("active");
+})
